07bf13dc2acd30a6c1c8efe580633cd89e4b5cea
Allan96 <allan.souza.silva@usp.br>
2019-07-25 23:29:03 -0300

    Ajustes no twitter feed

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,23 @@ $('.flutuante').click(function(e) {
     e.preventDefault();
     var isClass = $('.twitter-feed').hasClass('active');
     if (isClass) {
-        $('.twitter-feed').removeClass('active')
-        $('.twitter-feed').fadeOut();
+        $('.twitter-feed').removeClass('active');
+
+
+        $(".twitter-feed").animate({
+            left: "-100vw"
+        }, 1000, function() {
+            // Animation complete.
+        });
+
     } else {
         $('.twitter-feed').addClass('active');
-        $('.twitter-feed').fadeIn();
+
+        $(".twitter-feed").animate({
+            left: "0"
+        }, 1000, function() {
+            // Animation complete.
+        });
     }
 
 });
